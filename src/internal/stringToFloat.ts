43a8fd16c1baf6972243ec66ad14fdc6689a50a5
Jason Gleman <jason.gleman@gmail.com>
2023-01-14 20:02:24 +0000

    make these functions more compact

--- src/internal/stringToFloat.ts
+++ src/internal/stringToFloat.ts
@@ -1,6 +1,5 @@
 export function stringToFloat(numAsString: string): number {
   const asFloat = parseFloat(numAsString);
   // if the input string is a floating point number that is equal to 1, return 100
-  if (numAsString.indexOf(".") !== -1 && asFloat === 1) return 100;
-  return asFloat;
+  return numAsString.indexOf(".") !== -1 && asFloat === 1 ? 100 : asFloat;
 }
