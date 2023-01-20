c73ddc42bc32b1dd6160d3be77233ced71e2528f
Jason Gleman <jason.gleman@gmail.com>
2023-01-20 21:49:14 +0000

    feat: add comment about limitation of parsing percentage values

--- src/internal/handlePercentInput.ts
+++ src/internal/handlePercentInput.ts
@@ -1,8 +1,15 @@
 import { stringToFloat } from "./stringToFloat";
 
 // if the input is a string, convert it to a float, if the string resolves to
-// equal 1, we return 100 (so 1.0 will be handled as 100%, if not, we return the value
+// equal 1, we return 100 (so 1.0 will be handled as 100%, if not, we return the
+//  value.
+
+// There is a known limitation here, it doesn't appear to be possible in
+// JavaScript to differentiate between 1.0 and 1 if the type is a numnber.
+// if the user enters a string, we are fine, 1.0 will be treated as 100% as
+// expected, if the user enters 1.0 as a number, it will be treated as 1%.
 export function handlePercentInput(input: number | string): number {
+  if (typeof input === "number" && input === 1) return input / 100;
   const nAsFloat = typeof input === "string" ? stringToFloat(input) : input;
   return nAsFloat > 1 ? nAsFloat / 100 : nAsFloat;
 }
