e785d15808b33a8c32fe95796f3e518b3ef34d41
Jason Gleman <jason.gleman@gmail.com>
2023-01-20 21:48:54 +0000

    fix: bug where alpha value of 0 was treated as no alpha value

--- src/hslString.ts
+++ src/hslString.ts
@@ -10,7 +10,10 @@ export function hslString(color: Color): string {
   const h1 = Math.round(color.hsl.h);
   const s1 = Math.round(color.hsl.s * 100);
   const l1 = Math.round(color.hsl.l * 100);
-  const a1 = color.hsl.a ? Math.round(color.hsl.a * 100) : undefined;
+  const a1 =
+    typeof color.hsl.a !== "undefined"
+      ? Math.round(color.hsl.a * 100)
+      : undefined;
 
   if (typeof a1 !== "undefined") {
     return `hsl(${h1} ${s1}% ${l1}% / ${a1}%)`;
--- src/rgbString.ts
+++ src/rgbString.ts
@@ -10,7 +10,10 @@ export function rgbString(color: Color): string {
   const r1 = Math.round(color.rgb.r);
   const g1 = Math.round(color.rgb.g);
   const b1 = Math.round(color.rgb.b);
-  const a1 = color.rgb.a ? Math.round(color.rgb.a * 100) / 100 : 0;
+  const a1 =
+    typeof color.rgb.a !== "undefined"
+      ? Math.round(color.rgb.a * 100) / 100
+      : 0;
 
   if (typeof color.rgb.a !== "undefined") {
     return `rgb(${r1} ${g1} ${b1} / ${a1})`;
