2d2ccddb28185d2fc0d6181f032446a541170f2d
Jason Gleman <jason.gleman@gmail.com>
2026-03-11 12:02:17 +0100

    chore: updates to package.json, remove unused globals from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -21,10 +21,6 @@ export default [
         ecmaVersion: "latest",
         sourceType: "module",
       },
-      globals: {
-        window: "readonly",
-        document: "readonly",
-      },
     },
 
     plugins: {
